349c823f12efc49545b8772fa83489f4d996881b
FixedLocally <9797160+FixedLocally@users.noreply.github.com>
2026-05-04 13:31:10 +0100

    fix: make it possible to use separate env vars for the db connection

--- helpers/db.ts
+++ helpers/db.ts
@@ -1,6 +1,14 @@
 import {createPool} from "mysql2/promise";
 
-const dbPool = createPool(process.env.MYSQL!);
+function connectionString() {
+  if (process.env.MYSQL) {
+    return process.env.MYSQL;
+  } else {
+    return `mysql://${process.env.MYSQL_USERNAME}:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT ?? 3306}/${process.env.MYSQL_DATABASE}`;
+  }
+}
+
+const dbPool = createPool(connectionString());
 
 export function getDbConnection() {
   return dbPool.getConnection();
