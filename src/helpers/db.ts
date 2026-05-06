d464542c783c4d322b98e00529bf9fc837fe618d
FixedLocally <9797160+FixedLocally@users.noreply.github.com>
2026-05-06 19:52:08 +0100

    fix: return snowflake as strings

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -1,15 +1,19 @@
 import {createPool} from "mysql2/promise";
 
 function connectionString() {
-  if (process.env.MYSQL) {
-    return process.env.MYSQL;
-  } else {
-    return `mysql://${process.env.MYSQL_USERNAME}:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT ?? 3306}/${process.env.MYSQL_DATABASE}`;
-  }
+    if (process.env.MYSQL) {
+        return process.env.MYSQL;
+    } else {
+        return `mysql://${process.env.MYSQL_USERNAME}:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT ?? 3306}/${process.env.MYSQL_DATABASE}`;
+    }
 }
 
-const dbPool = createPool(connectionString());
+const dbPool = createPool({
+    uri: connectionString(),
+    supportBigNumbers: true,
+    bigNumberStrings: true,
+});
 
 export function getDbConnection() {
-  return dbPool.getConnection();
+    return dbPool.getConnection();
 }
