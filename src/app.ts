c68c7dc9c4e15c6b1cc1258e0ddea28ac0fefbef
FixedLocally <9797160+FixedLocally@users.noreply.github.com>
2026-05-06 04:43:03 +0100

    dep: add cookie parser

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import routes from "@/routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use(routes);
 
 export default app;
