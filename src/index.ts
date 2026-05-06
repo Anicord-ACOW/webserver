ada961fc3c06fb0e89a8a58f4d8348914553c3ce
FixedLocally <9797160+FixedLocally@users.noreply.github.com>
2026-05-06 04:42:42 +0100

    fix: load .env before anything else

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import app from "@/app";
-
 process.loadEnvFile(".env");
 
-app.listen(3000, (err) => {
-  if (err) throw err;
-  console.log("Server is running on port 3000");
+void import("@/app").then(({default: app}) => {
+  app.listen(3000, (err) => {
+    if (err) throw err;
+    console.log("Server is running on port 3000");
+  });
 });
