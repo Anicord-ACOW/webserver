9a9ef5eb73ed34bdbd735f060ae20bef97054f44
FixedLocally <9797160+FixedLocally@users.noreply.github.com>
2026-05-14 21:50:47 +0100

    feat: add swagger doc page

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express, {Request, Response, NextFunction} from "express";
 import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
 import routes from "@/routes";
 import {getEntityManager} from "@/helpers/db";
 import {APIError} from "@/helpers/api-error";
+
+import swaggerDocument from "@/swagger.json";
 
 const app = express();
 
 app.use(express.json({limit: "50kb"}));
 app.use(express.urlencoded({ extended: true, limit: "50kb" }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use((req, res, next) => {
     req.em = getEntityManager();
     next();
